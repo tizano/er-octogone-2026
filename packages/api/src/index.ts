15507e6deeb6f010b2563ffa51297d56c8b1ce6f
Mathieu <ma.scarlatella@gmail.com>
2026-05-17 20:10:22 -0400

    fix: add transformer

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,11 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC } from '@trpc/server';
+import superjson from 'superjson';
 
-import type { Context } from "./context";
+import type { Context } from './context';
 
-export const t = initTRPC.context<Context>().create();
+export const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+});
 
 export const router = t.router;
 
